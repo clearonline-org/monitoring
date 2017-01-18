df094a656e63e8dbb4848be5835b879bb96a9821
Marcellin Nshimiyimana <mars@fusemachines.com>
2017-01-18 02:02:49 -0500

    Optional commit message

---
+++ assets/country-time.d.ts
@@ -0,0 +1,10 @@
+/**
+* @Author: mars
+* @Date:   2016-12-29T19:58:18-05:00
+* @Last modified by:   mars
+* @Last modified time: 2016-12-29T20:02:13-05:00
+*/
+
+declare namespace CountryTime {
+  var now: any;
+}
